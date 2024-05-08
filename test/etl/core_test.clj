555f4c1548d5f66a1881a84cdb4b0206fa4c2050
Rahul Gaur <rahul.nbg@gmail.com>
2024-05-08 15:54:37 -0400

    chore: remove unused deps

--- test/etl/core_test.clj
+++ test/etl/core_test.clj
@@ -1,8 +1,6 @@
 (ns etl.core-test
   (:require [clojure.string :as s]
             [clojure.core.async :as a]
-            [cognitect.aws.client.api :as aws]
-            [etl.utils :refer [aws-client-invoke]]
             [etlp.core :as etlp]
             [etlp.utils.core :refer [wrap-record]]
             [etlp.processors.stdout :refer [create-stdout-destination!]]
